038cae2921249f8f58212d352e876322d2771b05
NTMinh-0802 <ntminh.cmnc@gmail.com>
2024-06-24 21:55:55 +0700

    update: feature send data from dht11 to thingsboard

---
+++ project_1_Minh/main/connect_mqtt.h
@@ -0,0 +1,42 @@
+#ifndef MQTT_EXAMPLE_H
+#define MQTT_EXAMPLE_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "dht11.h"
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+void publish_dht11_data(esp_mqtt_client_handle_t client);
+void log_error_if_nonzero(const char *message, int error_code);
+void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+void mqtt_app_start(void);
+void init_start_mqtt(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MQTT_EXAMPLE_H
--- project_1_Minh/main/main.c
+++ project_1_Minh/main/main.c
@@ -1,21 +1,13 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "spi_flash_mmap.h"
 #include "max30102.h"
-#include "wifi_mqtt.h"
+#include "dht11.h"
+#include "connect_wifi.h"
+#include "connect_mqtt.h"
 
-#define MAX_DATA_POINTS 100
-#define DATA_FILE "/spiffs/heartbeat.log"
 
 #define I2C_SDA 21
 #define I2C_SCL 22
@@ -24,8 +16,7 @@
 
 max30102_config_t max30102 = {};
 
-esp_err_t i2c_master_init(i2c_port_t i2c_port)
-{
+esp_err_t i2c_master_init(i2c_port_t i2c_port){
     i2c_config_t conf = {};
     conf.mode = I2C_MODE_MASTER;
     conf.sda_io_num = I2C_SDA;
@@ -37,17 +28,14 @@ esp_err_t i2c_master_init(i2c_port_t i2c_port)
     return i2c_driver_install(i2c_port, I2C_MODE_MASTER, 0, 0, 0);
 }
 
-void get_bpm(void* param)
-{
+void get_bpm(void* param) {
     printf("MAX30102 Test\n");
     max30102_data_t result = {};
     /*ESP_ERROR_CHECK(max30102_print_registers(&max30102));*/
-    while(true)
-    {
+    while(true) {
         //Update sensor, saving to "result"
         ESP_ERROR_CHECK(max30102_update(&max30102, &result));
-        if(result.pulse_detected)
-        {
+        if(result.pulse_detected) {
             printf("BEAT\n");
             printf("BPM: %f | SpO2: %f%%\n", result.heart_bpm, result.spO2);
         }
@@ -60,44 +48,47 @@ void get_bpm(void* param)
 
 void app_main(void)
 {
-    //Init I2C_NUM_0
-    ESP_ERROR_CHECK(i2c_master_init(I2C_PORT));
-    //Init sensor at I2C_NUM_0
-    ESP_ERROR_CHECK(max30102_init( &max30102, I2C_PORT,
-                   MAX30102_DEFAULT_OPERATING_MODE,
-                   MAX30102_DEFAULT_SAMPLING_RATE,
-                   MAX30102_DEFAULT_LED_PULSE_WIDTH,
-                   MAX30102_DEFAULT_IR_LED_CURRENT,
-                   MAX30102_DEFAULT_START_RED_LED_CURRENT,
-                   MAX30102_DEFAULT_MEAN_FILTER_SIZE,
-                   MAX30102_DEFAULT_PULSE_BPM_SAMPLE_SIZE,
-                   MAX30102_DEFAULT_ADC_RANGE, 
-                   MAX30102_DEFAULT_SAMPLE_AVERAGING,
-                   MAX30102_DEFAULT_ROLL_OVER,
-                   MAX30102_DEFAULT_ALMOST_FULL,
-                   false ));
+    // //Init I2C_NUM_0
+    // ESP_ERROR_CHECK(i2c_master_init(I2C_PORT));
+    // //Init sensor at I2C_NUM_0
+    // ESP_ERROR_CHECK(max30102_init( &max30102, I2C_PORT,
+    //                MAX30102_DEFAULT_OPERATING_MODE,
+    //                MAX30102_DEFAULT_SAMPLING_RATE,
+    //                MAX30102_DEFAULT_LED_PULSE_WIDTH,
+    //                MAX30102_DEFAULT_IR_LED_CURRENT,
+    //                MAX30102_DEFAULT_START_RED_LED_CURRENT,
+    //                MAX30102_DEFAULT_MEAN_FILTER_SIZE,
+    //                MAX30102_DEFAULT_PULSE_BPM_SAMPLE_SIZE,
+    //                MAX30102_DEFAULT_ADC_RANGE, 
+    //                MAX30102_DEFAULT_SAMPLE_AVERAGING,
+    //                MAX30102_DEFAULT_ROLL_OVER,
+    //                MAX30102_DEFAULT_ALMOST_FULL,
+    //                false ));
     
     // //Start test task
     // xTaskCreate(get_bpm, "Get BPM", 8192, NULL, 1, NULL);
 
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) 
+    //DHT11
+    DHT11_init(GPIO_NUM_25);
+
+    while(1) 
     {
+        printf("Temperature is %d \n", DHT11_read().temperature);
+        printf("Humidity is %d\n", DHT11_read().humidity);
+        printf("Status code is %d\n", DHT11_read().status);
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+
+    //Connect wifi
+        // Khởi tạo NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
-    // init WiFi and MQTT
-    wifi_init();
-    mqtt_app_start();
-
-    // simulation
-    int heart_rate = 80; // example
-    int spo2 = 95; // example
-
-    send_heartbeat_data(heart_rate, spo2);
-      //Start test task
-    xTaskCreate(get_bpm, "Get BPM", 8192, NULL, 1, NULL);
+    // Khởi tạo và kết nối WiFi
+    wifi_init_sta();
+    init_start_mqtt();
 }
